9447fe4c8ca726085235dff4a4ebe4c432d9592a
Vedant Shelar <vedantshelar871@gmail.com>
2025-08-17 20:55:49 +0530

    final commit

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -10,24 +10,24 @@ function Statistics() {
                     <div className={styles.statisticsOuterBox} data-aos="flip-up">
                         <div className="statisticsBox">
                             <i class="fa-solid fa-code"></i>
-                            <p className="statisticsBoxHead">Projects</p>
-                            <p className="statisticsBoxInfo">I have built over 7+ projects, each showcasing creativity, functionality, and problem-solving skills.</p>
+                            <p className={styles.statisticsBoxHead}>Projects</p>
+                            <p className={styles.statisticsBoxInfo}>I have built over 7+ projects, each showcasing creativity, functionality, and problem-solving skills.</p>
                         </div>
                         <p className="statisticsBoxNumber">7+</p>
                     </div>
                     <div className={styles.statisticsOuterBox} data-aos="flip-up">
                         <div className="statisticsBox">
                             <i class="fa-solid fa-trophy"></i>
-                            <p className="statisticsBoxHead">Tech Stack</p>
-                            <p className="statisticsBoxInfo">I have learned 11+ programming languages, each adding a unique layer to my problem-solving skills.</p>
+                            <p className={styles.statisticsBoxHead}>Tech Stack</p>
+                            <p className={styles.statisticsBoxInfo}>I have learned 11+ programming languages, each adding a unique layer to my problem-solving skills.</p>
                         </div>
                         <p className="statisticsBoxNumber">11+</p>
                     </div>
                     <div className={styles.statisticsOuterBox} data-aos="flip-up">
                         <div className="statisticsBox">
                             <i class="fa-solid fa-award"></i>
-                            <p className="statisticsBoxHead">Achievements</p>
-                            <p className="statisticsBoxInfo">Proudly earned 2+ achievements, including competition certificates and many more.</p>
+                            <p className={styles.statisticsBoxHead}>Achievements</p>
+                            <p className={styles.statisticsBoxInfo}>Proudly earned 2+ achievements, including competition certificates and many more.</p>
                         </div>
                         <p className="statisticsBoxNumber">2+</p>
                     </div>
